b7bf79d81822f19e05e16d6c58155f1495604462
erocoar <fj.tiedemann@googlemail.com>
2018-09-22 13:55:59 +0200

    add voter union class

--- R/vote_complex.R
+++ R/vote_complex.R
@@ -1,26 +1,42 @@
-setClass("Voting", 
-  slots = c(
-    voters = "character",
-    voter_positions = "Position",
-    sq = "Position",
-    dimension = "numeric"
-  )
-)
-
 # TODO: Lazyeval to get voter names here for `voters`
-setMethod("initialize", "Voting", 
-  function(.Object, voters, voter_positions, sq, dimension) {
-    .Object@voters <- voters
-    .Object@voter_positions <- NA
+.Voters <- setClass("Voters",
+  contains = "Position"
+)
+
+setMethod("initialize", "Voters",
+  function(.Object, position, dimension) {
+    .Object@position <- position
+    .Object@dimension <- dimension
+    .Object
   }
 )
 
-setMethod("+", signature(v1 = "Position", v2 = "Position"),
-  function(v1, v2) {
-    check_positions(v1, v2)
-  }
-)
+setMethod("+", signature = signature(e1 = "Voter", e2 = "Voter"),
+  function(e1, e2) {
+    check_positions(e1, e2)
+    .Voters(
+      position = cbind(e1@position, e2@position),
+      dimension = e1@dimension
+    )
+  })
 
+setMethod("+", signature = signature(e1 = "Voters", e2 = "Voter"),
+  function(e1, e2) {
+    check_positions(e1, e2)
+    .Voters(
+      position = cbind(e1@position, e2@position),
+      dimension = e1@dimension
+    )
+  })
+
+setMethod("+", signature = signature(e1 = "Voter", e2 = "Voters"),
+  function(e1, e2) {
+    check_positions(e1, e2)
+    .Voters(
+      position = cbind(e1@position, e2@position),
+      dimension = e1@dimension
+    )
+  })
 
 check_positions <- function(v1, v2) {
   if (!is.position(v1) || (nargs() == 2 && !is.position(v2))) {
@@ -30,4 +46,3 @@ check_positions <- function(v1, v2) {
     stop("Position objects must have equal dimensions.")
   }
 }
-
