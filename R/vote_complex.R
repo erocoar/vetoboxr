714857ba04029adf24d537782cef09a6eaa3e24d
erocoar <fj.tiedemann@googlemail.com>
2018-09-22 11:42:25 +0200

    Voting class

--- R/vote_complex.R
+++ R/vote_complex.R
@@ -1,3 +1,20 @@
+setClass("Voting", 
+  slots = c(
+    voters = "character",
+    voter_positions = "Position",
+    sq = "Position",
+    dimension = "numeric"
+  )
+)
+
+# TODO: Lazyeval to get voter names here for `voters`
+setMethod("initialize", "Voting", 
+  function(.Object, voters, voter_positions, sq, dimension) {
+    .Object@voters <- voters
+    .Object@voter_positions <- NA
+  }
+)
+
 setMethod("+", signature(v1 = "Position", v2 = "Position"),
   function(v1, v2) {
     check_positions(v1, v2)
@@ -13,3 +30,4 @@ check_positions <- function(v1, v2) {
     stop("Position objects must have equal dimensions.")
   }
 }
+
