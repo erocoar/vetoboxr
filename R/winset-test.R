29b887d0943079f8b88c81d69ad730169373819e
erocoar <fj.tiedemann@googlemail.com>
2018-11-07 16:18:37 +0100

    add winset area

--- R/winset-test.R
+++ R/winset-test.R
@@ -13,3 +13,12 @@ intersection = voter_list[[3]]
 for (v in seq(length(voter_list) - 1)) {
   intersection = rgeos::gIntersection(intersection, voter_list[[v]])
 }
+
+sq <- SQ(c(2, 4))
+voter1 <- Voter(c(3, 4), role = "Veto")
+voter2 <- Voter(c(1, 1), role = "Veto")
+voter3 <- Voter(c(7, 3), role = "AS")
+vote <- Vote(sq ~ voter1 + voter2 + voter3, iter = 1)
+
+vote$winset_area
+length(vote$winsets)
