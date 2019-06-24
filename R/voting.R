d26940cc388a8264db8d135a1ee8488671d72066
Frederik Tiedemann <fj.tiedemann@googlemail.com>
2019-06-24 22:55:24 +0200

    add c

--- R/voting.R
+++ R/voting.R
@@ -110,6 +110,7 @@ Vote.Voters <- function(
   x_idx <-  seq(1, voter_count * dimension, dimension)
 
   for (i in seq(1, iter)) {
+    cat(i, "\n")
     # 0. update sq-drift
     # TODO implement good SQ class checks
     # TODO sq_drift function argument by default n / dim?
